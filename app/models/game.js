21c9bcab831954b2afa4fd872f09bd8d1ff50b40
javieranselmi <javieranselmi@gmail.com>
2017-12-22 02:28:44 -0300

    Fixing game to instantiate correctly a board

--- app/models/game.js
+++ app/models/game.js
@@ -1,5 +1,14 @@
 let Board = require("./board");
 
-let currentGame = new Board();
+let currentGame = new Board(2, 2, 300);
 
-module.exports = currentGame;
+module.exports = {
+    board: currentGame,
+    reset: function() {
+        currentGame = new Board(2, 2, 300);
+        this.board = currentGame;
+    },
+    tileClickedAt: function(top,left) {
+        currentGame.tileClickedAt(top, left);
+    }
+};
