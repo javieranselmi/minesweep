90c5e9aef95aa9f185f4f7a653823f41a6fd19da
javieranselmi <javieranselmi@gmail.com>
2017-12-22 16:12:06 -0300

    Improving routes and fixing insecure port

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', gameRoutes);
+app.use('/api', gameRoutes);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,7 +40,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(6666, () => {
+app.listen(7210, () => {
   console.log("Listening!")
 });
 
